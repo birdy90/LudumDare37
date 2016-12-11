880756133eabaa5ffe041ae5be39c78cdb29dedf
Artemii <badabooom63@gmail.com>
2016-12-11 18:06:37 +0300

    Сцена с кнопками

---
+++ Assets/Scripts/Core/UIPlant.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class UIPlant : MonoBehaviour {
+    public Text UIText;
+    public Image UIImage;
+    public Plant PlantPrefab;
+    // Use this for initialization
+    void Start () {
+        UIText.text = PlantPrefab.Name+"\nCost: "+PlantPrefab.InitialBuyCost+"\nProfit: "+PlantPrefab.GrowthValueCost[0]+"-"+ PlantPrefab.GrowthValueCost[9];
+        UIImage.sprite = PlantPrefab.GrowStages[PlantPrefab.GrowStages.Length - 1];
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
