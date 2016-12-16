c5bcd2f5a33a7e57eff955f14479d93c865cdd69
ArtemiiFrolov <arfr@live.ru>
2016-12-16 15:00:22 +0300

    Новый баланс, изменено отображение цены

--- Assets/Scripts/Utils/UIPlant.cs
+++ Assets/Scripts/Utils/UIPlant.cs
@@ -11,7 +11,16 @@ public class UIPlant : MonoBehaviour {
     private Image _renderer;
 
     void Start () {
-        UIText.text = PlantPrefab.Name+"\nCost: "+PlantPrefab.InitialBuyCost+"\nProfit: "+PlantPrefab.GrowthValueCost[0]+"-"+ PlantPrefab.GrowthValueCost[9]+"\nTime: "+PlantPrefab.GrowthTime*PlantPrefab.OneStageGrowthTime;
+        if (PlantPrefab.InitialBuyCost>1000000)
+        {
+            UIText.text = PlantPrefab.Name + "\nCost: " + (double)PlantPrefab.InitialBuyCost/1000000 + "KK\nProfit: " + (double)PlantPrefab.GrowthValueCost[0]/1000000 + "KK-" + (double)PlantPrefab.GrowthValueCost[9]/1000000 + "KK\nTime: " + PlantPrefab.GrowthTime * PlantPrefab.OneStageGrowthTime;
+        }
+        else if (PlantPrefab.InitialBuyCost > 1000)
+        {
+            UIText.text = PlantPrefab.Name + "\nCost: " + (double)PlantPrefab.InitialBuyCost / 1000 + "K\nProfit: " + (double)PlantPrefab.GrowthValueCost[0] / 1000 + "K-" + (double)PlantPrefab.GrowthValueCost[9] / 1000 + "K\nTime: " + PlantPrefab.GrowthTime * PlantPrefab.OneStageGrowthTime;
+        }
+        else 
+            UIText.text = PlantPrefab.Name+"\nCost: "+PlantPrefab.InitialBuyCost+"\nProfit: "+PlantPrefab.GrowthValueCost[0]+"-"+ PlantPrefab.GrowthValueCost[9]+"\nTime: "+PlantPrefab.GrowthTime*PlantPrefab.OneStageGrowthTime;
         UIImage.sprite = PlantPrefab.ReadySprite;
         _renderer = GetComponent<Image>();
 
